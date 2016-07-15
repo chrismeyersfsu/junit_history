8f68b7b43969f16f0f796980c283c461447112da
Chris Meyers <chris.meyers.fsu@gmail.com>
2016-07-15 13:42:24 -0400

    added second detail graph

---
+++ client/graph_timestamp_duration.js
@@ -0,0 +1,56 @@
+var GraphTimestampDuration = function(data) {
+    this._graph = new Rickshaw.Graph( {
+        element: document.getElementById("chart_detail"),
+        renderer: 'scatterplot',
+        stroke: true,
+        width: 2000,
+        height: 500,
+        min: 'auto',
+        series: [{
+            data: data,
+            color: 'steelblue'
+        }],
+    }); 
+
+    this.format_x_axis = function(d) {
+        d = new Date(d)
+        return d3.time.format("%c")(d)
+    }
+    this._x_ticks = new Rickshaw.Graph.Axis.X({
+        graph: this._graph,
+        orientation: 'bottom',
+        element: document.getElementById('x_axis_detail'),
+        tickFormat: this.format_x_axis,
+        ticks: 10,
+    });
+
+    this._y_ticks = new Rickshaw.Graph.Axis.Y({
+        graph: this._graph,
+        tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+        ticksTreatment: 'glow',
+    });
+
+    this.handleFormatPointHover = function(series, x, y, formattedX, formattedY, d) {
+        var str = "Date: " + new Date(d.value.x) + "<br>";
+        str += "Duration: " + y + "<br>";
+        return str;
+    }
+    this._hover_detail = new Rickshaw.Graph.HoverDetail({
+        graph : this._graph,
+        formatter : this.handleFormatPointHover,
+    });
+
+    //this._graph.series[0].data = data;
+    this._graph.render(); // may not need this
+    this._graph.update();
+
+    this.update_data = function(data) {
+        console.log(data);
+        $('#chart_detail_title').text("Duration History for " + test_name(data[0].case));
+
+        this._graph.series[0].data = data;
+        this._graph.update();
+    }
+    return this;
+}
+
--- client/main.js
+++ client/main.js
@@ -1,8 +1,12 @@
 var _data = [];
 var _test_names = [];
 var _test_names_dict = [];
+var _test_name_ts_duration = [];
 var _graph;
+var _graph_detail;
 var _timestamps = [];
+var _slider;
+var _hover_detail;
 
 var format_x_axis = function(n) {
     return _test_names[n];
@@ -13,7 +17,7 @@ function test_name(_case) {
 }
 
 function populate_test_name(_case, test_names, test_names_dict) {
-    full_name = test_name(_case);
+    var full_name = test_name(_case);
     if (!test_names_dict[full_name]) {
         var len = test_names.push(full_name);
         test_names_dict[full_name] = Number(len - 1);
@@ -29,14 +33,33 @@ function populate_test_names(data, test_names, test_names_dict) {
             }
         });
     });
-
-    console.log(_test_names);
-    console.log(_test_names_dict);
 }
 function populate_timestamps(data, timestamps) {
      _.each(data, function(item) {
         timestamps.push((item['timestamp']));
     });
+}
+
+function populate_test_name_timestamp_duration(data, test_name_duration) {
+   _.each(data, function(item) {
+        _.each(item['junit']['suites'], function(suite) {
+            if (suite['name'] === "pytest") {
+                _.each(suite['cases'], function(_case) {
+                    var full_name = test_name(_case);
+                    if (!test_name_duration[full_name]) {
+                        test_name_duration[full_name] = []
+                    }
+                    test_name_duration[full_name].push({
+                        'x': item.timestamp,
+                        'y': _case.duration,
+                        'case': _case,
+                        'record': item,
+                    });
+                });
+            }
+        });
+    });
+   
 }
 
 function populate_x_y(data, test_names_dict) {
@@ -51,7 +74,7 @@ function populate_x_y(data, test_names_dict) {
                     if (full_name == "awx.main.tests.functional.test_db_credential::test_cred_unique_org_name_kind") {
                         console.log("Index: " + x_index);
                     }
-                    var entry = { 'x': x_index, 'y': _case['duration'] };
+                    var entry = { 'x': x_index, 'y': _case['duration'], 'case': _case, 'record': item };
                     points.push(entry);
                 });
             }
@@ -60,7 +83,36 @@ function populate_x_y(data, test_names_dict) {
     return _.sortBy(points, 'x');
 }
 
+function handleGraphClick(point) {
+    /*    
+    _graph.window.xMin = point.value.x - 1;
+    _graph.window.xMax = point.value.x + 1;
+    _graph.update();
+    _slider.update();
+    */
+    var full_name = test_name(point.value.case);
+    _graph_detail.update_data(_test_name_ts_duration[full_name]);
+    //_graph.update();
+}
+
+function handleAllDataButtonClick(e) {
+    var domain = _graph.dataDomain();
+    _graph.window.xMin = domain[0];
+    _graph.window.xMax = domain[1];
+    _graph.update();
+}
+
+function handleFormatPointHover(series, x, y, formattedX, formattedY, d) {
+    _hover_detail.last_hovered_point = d;
+    var date = new Date(d.value.record.timestamp);
+    var str = "Timestamp: " + new Date(d.value.record.timestamp) + "<br>";
+    str += "Test: " + _test_names[d.value.x] + "<br>";
+    str += "Duration: " + d.value.y + "<br>";
+    return str;
+}
+
 function generate_graph(data) {
+    var graphId = '#chart';
     _graph = new Rickshaw.Graph( {
         element: document.getElementById("chart"),
         renderer: 'scatterplot',
@@ -73,12 +125,14 @@ function generate_graph(data) {
             color: 'steelblue'
         }],
     }); 
+
     var x_ticks = new Rickshaw.Graph.Axis.X({
         graph: _graph,
         orientation: 'bottom',
         element: document.getElementById('x_axis'),
         //pixelsPerTick: 800,
-        tickFormat: format_x_axis
+        tickFormat: format_x_axis,
+        ticks: 10,
     });
     var y_ticks = new Rickshaw.Graph.Axis.Y({
         graph: _graph,
@@ -88,16 +142,22 @@ function generate_graph(data) {
 
     _graph.render();
 
-    var slider = new Rickshaw.Graph.RangeSlider({
+    _slider = new Rickshaw.Graph.RangeSlider({
         graph : _graph,
         element : document.getElementById('slider'),
     });
-    var hoverDetail = new Rickshaw.Graph.HoverDetail({
-        graph : _graph,
-        formatter : function(series, x, y, formattedX, formattedY, d) {
-            return _test_names[x] + " - " + y;
-        },
+    _slider.onSlide(function(x, y, z) {
     });
+    _hover_detail = new Rickshaw.Graph.HoverDetail({
+        graph : _graph,
+        formatter : handleFormatPointHover,
+    });
+
+    $(graphId).on('click', function() {
+        return handleGraphClick(_hover_detail.last_hovered_point);
+    });
+
+    _graph_detail = GraphTimestampDuration([{ x: 0, y: 0 }]);
     return _graph;
 }
 
@@ -106,18 +166,17 @@ $( document ).ready(function() {
     //$.get("sample_data.json", function(data) {
     $.get("/", function(data) {
         populate_timestamps(data, _timestamps);
+        populate_test_name_timestamp_duration(data, _test_name_ts_duration);
+        console.log(_test_name_ts_duration);
         populate_test_names(data, _test_names, _test_names_dict);
         points = populate_x_y(data, _test_names_dict);
         _data = points;
 
         generate_graph(_data);
-        console.log(points[0]);
-        console.log(points[1]);
-        console.log(points[2]);
-        console.log(points[3]);
-        console.log(points[4]);
     }).fail(function(reason) {
         console.log("Getting sample data failed " + reason);
         console.log(reason);
     });
+
+    $('#allDataButton').on('click', handleAllDataButtonClick);
 });
